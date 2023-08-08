99e2bd76b9e8e4a7922ef38daa0b0ee2eae386c6
FolukeBrown <olorunlosefoluke@gmail.com>
2023-08-08 05:59:59 +0200

    changes

--- edx2/carpooling.c
+++ edx2/carpooling.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
-int main(void){
-    int passenger;
-    double costOfGas;
-    double totalCost =0;
-    int i;
-    
-    scanf("%d", &passenger);
-    scanf("%lf", &costOfGas);
-    
-   
-    if (passenger == 0){
-         totalCost=costOfGas;
-          printf("%.2lf\n", totalCost);
-    }
-   
-    
-    else{
-    for (i=0; passenger > 0; i++){
-         
-    }
-    totalCost = (costOfGas+1.0)/passenger;
-   
-        printf("%.2lf\n", totalCost);
-    }
-       
-    
-    
-    return 0;
+/**
+* main - rogram should then print the cost that you have to pay (a decimal number) with 2 digits after the decimal point
+* Return: ALways 0 (Success)
+*/
+int main(void)
+{
+int passenger;
+double costOfGas;
+double totalCost = 0;
+
+scanf("%d", &passenger);
+scanf("%lf", &costOfGas);
+
+
+if (passenger == 0)
+{
+totalCost = costOfGas;
+printf("%.2lf\n", totalCost);
+}
+
+
+else if (passenger > 0)
+{
+totalCost = (costOfGas + 1.0) / (passenger + 1);
+
+printf("%.2lf\n", totalCost);
+}
+
+
+
+return (0);
 }
