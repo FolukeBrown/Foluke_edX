0af44a03f1e83aaf519a0fda898fa45ea2409db1
FolukeBrown <olorunlosefoluke@gmail.com>
2023-11-09 09:09:26 +0200

    update

--- edx2/test.py
+++ edx2/test.py
@@ -1,14 +1,2 @@
-num1, operator, num2 = input("Insert two numbers: ").split()
-num1 = float(num1)
-num2 = float(num2)
+print ('Hello Foluke')
 
-if operator == "+":
-    print("{} + {} = {}".format(num1, num2, num1 + num2))
-elif operator == "-":
-    print("{} - {} = {}".format(num1, num2, num1 - num2))
-elif operator == "*":
-    print("{} * {} = {}".format(num1, num2, num1 * num2))
-elif operator == "/":
-    print("{} / {} = {}".format(num1, num2, num1 / num2))
-else:
-    print("input a correct operator. Thank you")
